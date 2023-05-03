860c97698091505eb6e87441f0f88afca10812bc
unknown <loto99win@gmail.com>
2023-05-03 13:19:00 +0700

    update

---
+++ src/configs/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dpkqnwp9v',
+    api_key: '436481897595333',
+    api_secret: 'v_D-gGLSdmr71h2f7BlVfs1Hh1E'
+});
+
+module.exports = cloudinary;
